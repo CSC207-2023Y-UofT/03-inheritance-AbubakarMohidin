ec7e73b87cf10b14321f9c7c9e1ec03336c49f7e
abubakarmohidin <abubakar.mohidin@mail.utoronto.ca>
2023-06-10 20:41:00 -0400

    test 1

--- src/main/java/Bag.java
+++ src/main/java/Bag.java
@@ -6,89 +6,60 @@
  */
 
 public abstract class Bag {
-    /*
-     * TODO: Create the following private instance variables
-     *       - a String named color
-     *       - an int named numberOfContents
-     *       - an int named capacity
-     *       - an array of Strings named contents
-     */
+    private String color;
+    private int numberOfContents;
+    private int capacity;
+    private String[] contents;
 
-
-
-
-    /*
-     * TODO: Create a constructor that takes two arguments:
-     *       - a String representing the Bag's colour
-     *       - an int representing the Bag's capacity
-     *
-     * The other attributes (private instance variables) should
-     * be empty (e.g. numberOfContents is 0 and an empty String array for
-     * its contents.)
-     */
-
-
-
-
-    /*
-     * TODO: Create a variety of 'getter' functions.
-     *       These should be named:
-     *           - getColor
-     *           - getNumberOfContents
-     *           - getCapacity
-     */
-
-
-
-
-    /*
-     * TODO: Create a setter function called setColor which sets the
-     *       color of this bag to the given color.
-     */
-
-
-
-
-
-    /*
-     * TODO: Create a method called addItem that takes in a String
-     *       representing an item in the Bag.
-     *       The item is added into the Bag if the number of items
-     *       in the bag is < the capacity of the Bag.
-     *       Remember to modify numberOfContents accordingly.
-     *
-     *       This method should return true if the item was added
-     *       and false otherwise.
-     */
-
-
-
-
-
-    /**
-     * TODO: Create a method called popItem that returns a String.
-     *       The string should be the last item added to this Bag
-     *       and the item should be removed from this Bag.
-     *       Remember to modify numberOfContents accordingly.
-     *
-     * If there are no items in this Bag, return null.
-     *
-     * @return
-     */
-
-
-
-
-
-    /**
-     * Increase this bag's capacity by n.
-     *
-     * @param n the amount to increase this Bag's capacity by
-     */
-    public void increaseCapacity(int n) {
-        // TODO: Implement this method.
-
+    public Bag(String color, int capacity) {
+        this.color = color;
+        this.capacity = capacity;
+        this.numberOfContents = 0;
+        this.contents = new String[capacity];
     }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getNumberOfContents() {
+        return this.numberOfContents;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean addItem(String item) {
+        if (numberOfContents < capacity) {
+            contents[numberOfContents] = item;
+            numberOfContents++;
+            return true;
+        }
+        return false;
+    }
+
+    public String popItem() {
+        if (numberOfContents > 0) {
+            String item = contents[numberOfContents - 1];
+            contents[numberOfContents - 1] = null;
+            numberOfContents--;
+            return item;
+        }
+        return null;
+    }
+
+    public void increaseCapacity(int n) {
+        this.capacity += n;
+        String[] newContents = new String[capacity];
+        System.arraycopy(contents, 0, newContents, 0, numberOfContents);
+        contents = newContents;
+    }
+
 
     /**
      * Return the details of this Bag.
--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -5,29 +5,23 @@
  * You may find the readings in chapter 3. Relationships between Classes
  * helpful while working through this exercise.
  */
-class BagMain {
-    /**
-     * TODO: Implement this method
-     * Enhance each of the bags in bags. If double_enhance_handbags is
-     * True, then enhance any HandBags a second time.
-     *
-     * This method should work for *all* Bags! We will test this method
-     * on new Bag types (and HandBag subclasses)!
-     */
+public class BagMain {
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
-        // TODO: Implement this.
+        for (Bag bag : bags) {
+            bag.enhance();
+            if (double_enhance_handbags && bag instanceof HandBag) {
+                bag.enhance();
+            }
+        }
     }
 
-    /**
-     * TODO: Implement this method
-     * Return the total number of straps in CrossbodyBags in bags.
-     * Any bag that is not a CrossbodyBag should not be included
-     * in the count!
-     *
-     * @param bags An array of Bags
-     * @return The total number of straps of CrossbodyBags.
-     */
     public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
+        int totalStraps = 0;
+        for (Bag bag : bags) {
+            if (bag instanceof CrossbodyBag) {
+                totalStraps += ((CrossbodyBag) bag).getNumberOfStraps();
+            }
+        }
+        return totalStraps;
     }
 }
